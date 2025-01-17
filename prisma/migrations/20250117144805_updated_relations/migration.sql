367db03b7bd20d8e378440329ca1ea2706d4a402
va24nsh <vanshmaurya17@gmail.com>
2025-01-17 20:36:18 +0530

    Purchases Added

---
+++ prisma/migrations/20250117144805_updated_relations/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Purchase" ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'PENDING',
+ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION;
+
+-- AddForeignKey
+ALTER TABLE "Purchase" ADD CONSTRAINT "Purchase_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Purchase" ADD CONSTRAINT "Purchase_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { connectDB } = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const companyRoutes = require('./routes/company');
 const sellRoutes = require('./routes/sellRoutes');
-
+const purchaseRoutes = require('./routes/purchase');
 const app = express();
 
 // Middleware
@@ -34,6 +34,7 @@ connectDB()
 app.use('/api/users', userRoutes);
 app.use('/api/company', companyRoutes);
 app.use('/api/sell', sellRoutes);
+app.use('/api/purchase', purchaseRoutes);
 
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
